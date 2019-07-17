065cf0b9be0c8574353532681304cf15c3a8ebdc
Siham <sihamh@google.com>
2019-07-17 14:27:53 -0500

    Update hogwarts_models.py

--- hogwarts_models.py
+++ hogwarts_models.py
@@ -29,10 +29,10 @@ class Course(ndb.Model):
       location = ndb.StringProperty()
 
 # Enrollment tells us which student is enrolled in each class
-# student_id is the id of the student so there is a corresponding Student entry
+# student_num is the num of the student so there is a corresponding Student entry
 # with Student.student_num == Enrollment.student_num
-# course_name is the name of the class they are in, so there is Course entry
-# with Course.name == Enrollment.course_name
+# course_name is the name of the class they are in, so there is a corresponding
+# Course entry with Course.name == Enrollment.course_name
 class Enrollment(ndb.Model):
       student_num = ndb.IntegerProperty(required=True)
       course_name = ndb.StringProperty(required=True)
